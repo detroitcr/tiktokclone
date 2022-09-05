efa2cbf275aa11a8319567ec71c1d345309c8a62
Vikas Kumar <46410948+detroitcr@users.noreply.github.com>
2022-09-05 13:02:29 +0530

    pubspecupdated

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+class CustomColor{
+  // object of upper class
+  CustomColor._();
+  static const backGroundColor = Colors.black;
+  static const buttonColor = Color.fromARGB(255, 239, 83, 80);
+  static const borderColor = Colors.grey;
+}
---
+++ lib/view/Screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tiktokappclone/colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.backGroundColor,
+      body: Container(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
